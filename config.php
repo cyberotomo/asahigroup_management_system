acb91c1e97bf893320aa2884838f93660c8b6fe5
cyberotomo <cyberotomo@gmail.com>
2023-09-17 03:20:54 +0900

    修正

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+define('DSN', 'mysql:host=127.0.0.1;dbname=cyberotomo_asahigroupadmin');
+define('DB_USER', 'root');
+define('DB_PASS', 'pikatyu2929');
+?>
--- index.php
+++ index.php
@@ -1,10 +1,36 @@
+<html>
+<head><title>PHP TEST</title></head>
+<body>
+
 <?php
+require_once('config.php');
 
-function sum($x, $y){
-    $result = $x + $y;
-    return $result;
-}
-echo sum(100,200);
+//データベースへ接続、テーブルがない場合は作成
+try {
+    $pdo = new PDO(DSN, DB_USER, DB_PASS);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->exec("create table if not exists userData(
+        id int not null auto_increment primary key,
+        email varchar(255),
+        password varchar(255),
+        created timestamp not null default current_timestamp
+      )");
+  } catch (Exception $e) {
+    echo $e->getMessage() . PHP_EOL;
+  }
 
+ // レコード抽出
+ $sql = "SELECT * FROM userData";
+ $stmt = $pdo->query($sql);
+
+ echo "<ul>";
+ foreach($stmt as $row) {
+     echo "<li>" . $row['email'] . "</li>";
+ }
+ echo "</ul>";
+
+ $pdo = null;
 
 ?>
+</body>
+</html>
